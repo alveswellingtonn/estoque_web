e0452583eacbfebaff8f1b98e676bf57d69f9434
Wellington Alves <alveswellingtonmoraes@hotmail.com>
2023-11-03 14:04:37 -0300

    Criando a página de cadastro de categoria

--- src/page/newCategory/NewCategory.jsx
+++ src/page/newCategory/NewCategory.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import './NewCategory.css';
 
+import Input from '../../components/form/Input';
+import SubmitButton from '../../components/form/SubmitButton';
+
 function NewCategory() {
   return (
-    <div className='container'>NewCategory</div>
+    <main className='new_category'>
+      
+      <div className='title'>
+        <h1>Cadastrar Categoria</h1>
+      </div>
+
+      <form>
+
+        <Input
+          type='text' text='Nome da Categoria' name='name' placeholder='Insira o nome da categoria'
+        />
+
+        <SubmitButton text='Cadastrar'></SubmitButton>
+
+      </form>
+
+    </main>
   )
 }
 
