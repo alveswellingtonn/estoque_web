9b2f59f999cc29d93a9269420021cf9100318b85
Wellington Alves <alveswellingtonmoraes@hotmail.com>
2023-11-03 13:48:31 -0300

    Acrescentando novos menus

--- src/components/sideBar/Sidebar.jsx
+++ src/components/sideBar/Sidebar.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './Sidebar.css';
 import { Link } from "react-router-dom";
 
-import { FaHouseChimney, FaCartShopping, FaCartPlus} from "react-icons/fa6";
+import {
+  FaHouseChimney, FaCartShopping, FaCartPlus, FaBoxesStacked, FaChartLine, FaCirclePlus,
+  FaFolderPlus, FaFolder, FaGrip, FaGripLines, FaListCheck, FaListUl, FaList, FaRectangleList,
+  FaSquarePlus, FaTableList, FaRegRectangleList, FaRegSquarePlus
+} from "react-icons/fa6";
 
 
 
@@ -12,20 +16,32 @@ function Sidebar() {
       <ul>
         <Link to="/" className="link">
           <li className="sidebarListItem active">
-          <FaHouseChimney className='sidebarListIcon'/>
+            <FaHouseChimney className='sidebarListIcon' />
             Home
           </li>
         </Link>
         <Link to="/product" className="link">
           <li className="sidebarListItem active">
-          <FaCartShopping className='sidebarListIcon'/>
+            <FaBoxesStacked className='sidebarListIcon' />
             Produto
           </li>
         </Link>
         <Link to="/new-product" className="link">
           <li className="sidebarListItem active">
-          <FaCartPlus className='sidebarListIcon'/>
-            Novo Produto
+            <FaSquarePlus className='sidebarListIcon' />
+            Cadastrar Produto
+          </li>
+        </Link>
+        <Link to="/category" className="link">
+          <li className="sidebarListItem active">
+            <FaListUl className='sidebarListIcon' />
+            Categorias
+          </li>
+        </Link>
+        <Link to="/new-category" className="link">
+          <li className="sidebarListItem active">
+            <FaSquarePlus className='sidebarListIcon' />
+            Cadastrar Categoria
           </li>
         </Link>
       </ul>
