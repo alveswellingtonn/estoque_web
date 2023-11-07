feb08fc111f3e10dd049e9a02ce019f25eaeeac5
Wellington Alves <alveswellingtonmoraes@hotmail.com>
2023-11-07 09:54:13 -0300

    Desenvolvimento da página de listagem de categorias

--- src/page/category/Category.jsx
+++ src/page/category/Category.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Category.css';
 
 function Category() {
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://parseapi.back4app.com/parse/functions/get-category', {
+      method: 'POST',
+      headers: {
+        'X-Parse-Application-Id': 'G5UQZkgY9ppEKKiXdjzAmNZQMsMJZeZyHX9qSaqO',
+        'X-Parse-REST-API-Key': 'U3KlIUQrP7bHZEFOzaV65Jxcgd0nesPdC4K6pjhb',
+      }
+    })
+      .then((resp) => resp.json())
+      .then(data => {
+        setCategories(data.result)
+        console.log(data.result)
+      })
+      .catch(err => console.log(err))
+  }, []);
+
+
   return (
-    <div className='container'>Category</div>
+    <div className='category'>
+
+      <div className='container_category'>
+        <h1>Categorias</h1>
+      </div>
+
+      <div className="table_category">
+
+        <table>
+          <tr>
+            <th>Categoria</th>
+          </tr>
+          {
+            categories.map((category) => (
+              <tr key={category.objectId}>
+                <td>
+                  {category.name}
+                </td>
+              </tr>
+            ))
+          }
+        </table>
+
+      </div>
+
+    </div>
   )
 }
 
