3b5dc9ad171f24bb0a2c8f181279f1ee4cf17f18
Wellington Alves <alveswellingtonmoraes@hotmail.com>
2024-01-09 16:22:42 -0300

    Adicionando valores na caixa SELECT

--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -7,6 +7,9 @@ function Select( {text, name, options, handleOnChange, value} ) {
         <label htmlFor={name}>{text}:</label>
         <select name={name} id={name} onChange={handleOnChange} value={value}>
             <option>Selecione uma opção</option>
+            {options.map((option) => (
+              <option value={option.objectId} key={option.objectId}>{option.name}</option>
+            ))}
         </select>
     </div>
   )
--- src/page/newProduct/NewProduct.jsx
+++ src/page/newProduct/NewProduct.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './NewProduct.css'
 import Input from '../../components/form/Input';
 import SubmitButton from '../../components/form/SubmitButton';
 import Select from '../../components/form/Select';
 
 function NewProduct() {
+
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://parseapi.back4app.com/parse/functions/get-category', {
+      method: 'POST',
+      headers: {
+        'X-Parse-Application-Id': 'G5UQZkgY9ppEKKiXdjzAmNZQMsMJZeZyHX9qSaqO',
+        'X-Parse-REST-API-Key': 'U3KlIUQrP7bHZEFOzaV65Jxcgd0nesPdC4K6pjhb',
+      }
+    })
+      .then((resp) => resp.json())
+      .then(data => {
+        setCategories(data.result)
+        console.log(data.result)
+      })
+      .catch(err => console.log(err))
+  }, []);
+
+
   return (
     <main className='new_product'>
 
@@ -29,7 +50,7 @@ function NewProduct() {
           type='number' text='Quantidade do Produto' name='name' placeholder='Insira a quantidade de produto, ex: 99'
         />
 
-        <Select name='category_id' text='Selecione a categoria' options='cateogories {categories}'
+        <Select name='category_id' text='Selecione a categoria' options= {categories}
           handleOnChange='{handleCategory}' value='{project.category}'
         ></Select>
 
