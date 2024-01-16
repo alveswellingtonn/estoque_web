3e0228e49af24d15c95a0ac079229fa123888384
Wellington Alves <alveswellingtonmoraes@hotmail.com>
2024-01-16 16:16:10 -0300

    Salvando um produto

--- src/page/newProduct/NewProduct.jsx
+++ src/page/newProduct/NewProduct.jsx
@@ -6,8 +6,69 @@ import Select from '../../components/form/Select';
 
 function NewProduct() {
 
+  const [nameProduct, setNameProduct] = useState([]);
+  const [descriptionProduct, setDescriptionProduct] = useState([]);
+  const [priceProduct, setPriceProduct] = useState([]);
+  const [stockProduct, setStockProduct] = useState([]);
 
   const [categories, setCategories] = useState([]);
+
+  function createProduct() {
+
+    fetch('https://parseapi.back4app.com/parse/functions/create-product', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+          'X-Parse-Application-Id': 'G5UQZkgY9ppEKKiXdjzAmNZQMsMJZeZyHX9qSaqO',
+          'X-Parse-REST-API-Key': 'U3KlIUQrP7bHZEFOzaV65Jxcgd0nesPdC4K6pjhb',
+        },
+        body: JSON.stringify(
+          {
+            name: nameProduct,
+            description: descriptionProduct,
+            price: +priceProduct,
+            stock: +stockProduct,
+          }
+        ),
+      })
+      .then(resp => resp.json())
+      .then((data) => {
+        console.log(data)
+        // console.log('Nome produto é: ' + nameProduct)
+        // console.log('Descricao: ' + descriptionProduct)
+        // console.log('Preco: ' + priceProduct)
+        // console.log('Quantidade: ' + stockProduct)
+      })
+      .catch((err) => console.log(err))
+  }
+
+  const submit = (e) => {
+    e.preventDefault();
+    // console.log('Produto é:' + nameProduct)
+    // console.log('Descricao: ' + descriptionProduct)
+    // console.log('Preco: ' + priceProduct)
+    // console.log('Quantidade: ' + stockProduct)
+    createProduct()
+  }
+
+  function handleName (e) {
+    setNameProduct(e.target.value);
+    // console.log('Valor Input nome: ' + nameProduct)
+  }
+  function handleDescription (e) {
+    setDescriptionProduct(e.target.value);
+    // console.log('Valor Input descricao: ' + descriptionProduct)
+  }
+
+  function handlePrice (e) {
+    setPriceProduct(e.target.value);
+    // console.log('Valor Input preco: ' + priceProduct)
+  }
+
+  function handleStock (e) {
+    setStockProduct(e.target.value);
+    // console.log('Valor Input stock: ' + stockProduct)
+  }
 
   useEffect(() => {
     fetch('https://parseapi.back4app.com/parse/functions/get-category', {
@@ -33,21 +94,25 @@ function NewProduct() {
         <h1>Cadastrar Produto</h1>
       </div>
 
-      <form>
+      <form onSubmit={submit}>
         <Input
           type='text' text='Nome do Produto' name='name' placeholder='Insira o nome do produto'
+          handleOnChange={handleName}
         />
 
         <Input
           type='text' text='Descrição do Produto' name='name' placeholder='Insira a descrição do produto'
+          handleOnChange={handleDescription}
         />
 
         <Input
           type='number' text='Valor do Produto' name='name' placeholder='Insira o valor do produto, ex: 99.99'
+          handleOnChange={handlePrice}
         />
 
         <Input
           type='number' text='Quantidade do Produto' name='name' placeholder='Insira a quantidade de produto, ex: 99'
+          handleOnChange={handleStock}
         />
 
         <Select name='category_id' text='Selecione a categoria' options= {categories}
